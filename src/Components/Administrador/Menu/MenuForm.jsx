61f517e284e76c853fc8824a5c364df999945788
unknown <luqita.dmayo@gmail.com>
2023-11-09 16:36:35 -0300

    Aguregue validaciones y estilos

--- src/Components/Administrador/Menu/MenuForm.jsx
+++ src/Components/Administrador/Menu/MenuForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Col, Container, Row } from 'react-bootstrap';
 
 const MenuForm = ({
   formDataMenu,
@@ -23,9 +23,8 @@ const MenuForm = ({
       name: formDataMenu.name ? '' : 'El nombre es obligatorio',
       price: formDataMenu.price ? '' : 'El precio es obligatorio',
       category: formDataMenu.category ? '' : 'Seleccione una categoría',
-      detail: formDataMenu.detail.length<=200 ? '' : 'El maximo son 200 caracteres',
-      detail: formDataMenu.detail.length>=10 ? '' : 'El minimo son 10 caracteres',
-
+      detail: formDataMenu.detail.length <= 200 ? '' : 'El máximo son 200 caracteres',
+      detail: formDataMenu.detail.length >= 10 ? '' : 'El mínimo son 10 caracteres',
     };
 
     setFormErrors(errors);
@@ -40,78 +39,82 @@ const MenuForm = ({
   };
 
   return (
-    <div className='m-5'>
-      <Form>
-        <Form.Group>
-          <Form.Label>Nombre:</Form.Label>
-          <Form.Control
-            type="text"
-            name="name"
-            placeholder="Nombre"
-            value={formDataMenu.name}
-            onChange={handleChange}
-          />
-          <small className="text-danger">{formErrors.name}</small>
-        </Form.Group>
-        <Form.Group>
-          <Form.Check
-            type="checkbox"
-            name="isAvailable"
-            checked={formDataMenu.isAvailable}
-            onChange={handleChange}
-            label="Disponible"
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Precio:</Form.Label>
-          <Form.Control
-            type="number"
-            name="price"
-            placeholder="Precio"
-            value={formDataMenu.price}
-            onChange={handleChange}
-          />
-          <small className="text-danger">{formErrors.price}</small>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Detalle:</Form.Label>
-          <Form.Control
-            type="text"
-            name="detail"
-            placeholder="Detalle"
-            value={formDataMenu.detail}
-            onChange={handleChange}
-          />
-          <small className="text-danger">{formErrors.detail}</small>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Categoría:</Form.Label>
-          <Form.Control
-            as="select"
-            name="category"
-            value={formDataMenu.category}
-            onChange={handleChange}
-          >
-            <option value="">Seleccione una categoría</option>
-            {categoriesOptions.map((category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-          </Form.Control>
-          <small className="text-danger">{formErrors.category}</small>
-        </Form.Group>
-        <Button variant="success" onClick={handleFormSubmit}>
-          {editingMenuId !== null ? 'Guardar' : 'Enviar'}
-        </Button>
-        {editingMenuId !== null && (
-          <Button variant="secondary m-1" onClick={handleCancelEdit}>Cancelar</Button>
-        )}
-        {editingMenuId === null && (
-          <Button variant="secondary m-1" onClick={handleCancelAddMenu}>Cancelar</Button>
-        )}
-      </Form>
-    </div>
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+      <Col xs={10} sm={10} md={6} lg={6}>
+        <Form>
+          <Form.Group>
+            <Form.Label>Nombre:</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              placeholder="Nombre"
+              value={formDataMenu.name}
+              onChange={handleChange}
+            />
+            <small className="text-danger">{formErrors.name}</small>
+          </Form.Group>
+          <Form.Group>
+            <Form.Check
+              type="checkbox"
+              name="isAvailable"
+              checked={formDataMenu.isAvailable}
+              onChange={handleChange}
+              label="Disponible"
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Precio:</Form.Label>
+            <Form.Control
+              type="number"
+              name="price"
+              placeholder="Precio"
+              value={formDataMenu.price}
+              onChange={handleChange}
+            />
+            <small className="text-danger">{formErrors.price}</small>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Detalle:</Form.Label>
+            <Form.Control
+              type="text"
+              name="detail"
+              placeholder="Detalle"
+              value={formDataMenu.detail}
+              onChange={handleChange}
+            />
+            <small className="text-danger">{formErrors.detail}</small>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Categoría:</Form.Label>
+            <Form.Control
+              as="select"
+              name="category"
+              value={formDataMenu.category}
+              onChange={handleChange}
+            >
+              <option value="">Seleccione una categoría</option>
+              {categoriesOptions.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </Form.Control>
+            <small className="text-danger">{formErrors.category}</small>
+          </Form.Group>
+          <Button variant="success" onClick={handleFormSubmit}>
+            {editingMenuId !== null ? 'Guardar' : 'Enviar'}
+          </Button>
+          {editingMenuId !== null && (
+            <Button variant="secondary m-1" onClick={handleCancelEdit}>Cancelar</Button>
+          )}
+          {editingMenuId === null && (
+            <Button variant="secondary m-1" onClick={handleCancelAddMenu}>Cancelar</Button>
+          )}
+        </Form>
+      </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/Components/Administrador/Usuarios/UserForm.jsx
+++ src/Components/Administrador/Usuarios/UserForm.jsx
@@ -1,93 +1,110 @@
-import React from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
 const UserForm = ({ formData, handleChange, handleSubmit, handleCancel }) => {
-  const { name, email, password } = formData;
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
 
-  const isFormValid = () => {
-    const isNameValid = name.trim() !== '';
-    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email); // Validación de email
-    const isPasswordValid = password.trim().length >= 6; // Validación de longitud de contraseña
+  const validateForm = () => {
+    const errors = {
+      name: (formData.name.length >= 6 && formData.name.length <= 50) ? '' : 'El nombre debe tener entre 6 y 50 caracteres',
+      email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email) ? '' : 'Ingrese un email válido',
+      password: (formData.password.length >= 6 && formData.password.length <= 20) ? '' : 'La contraseña debe tener entre 6 y 20 caracteres',
+    };
 
-    return isNameValid && isEmailValid && isPasswordValid;
+    setFormErrors(errors);
+
+    return Object.values(errors).every((error) => error === '');
+  };
+
+  const handleFormSubmit = () => {
+    if (validateForm()) {
+      handleSubmit();
+    }
   };
 
   return (
-    <div className='m-5'>
-      <Form>
-        <Form.Group>
-          <Form.Label>Nombre:</Form.Label>
-          <Form.Control
-            type="text"
-            name="name"
-            placeholder="Nombre"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Email:</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={formData.email}
-            onChange={handleChange}
-            className="form-control"
-            required
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Check
-            type="checkbox"
-            name="isActive"
-            checked={formData.isActive}
-            onChange={handleChange}
-            label="Activo"
-            className="form-check"
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Contraseña:</Form.Label>
-          <Form.Control
-            type="text"
-            name="password"
-            placeholder="Contraseña"
-            value={formData.password}
-            onChange={handleChange}
-            className="form-control"
-            required
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Check
-            type="checkbox"
-            name="isAdmin"
-            checked={formData.isAdmin}
-            onChange={handleChange}
-            label="Administrador"
-            className="form-check"
-          />
-        </Form.Group>
-        <Button
-          className='btn btn-success m-1'
-          onClick={handleSubmit}
-          disabled={!isFormValid()} // Deshabilita el botón si el formulario no es válido
-        >
-          {formData.editingUserId !== null ? 'Guardar' : 'Enviar'}
-        </Button>
-        <Button className='btn btn-secondary m-1' onClick={handleCancel}>
-          Cancelar
-        </Button>
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col xs={10} sm={10} md={6} lg={6}>
+          <Form>
+            <Form.Group>
+              <Form.Label>Nombre:</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                placeholder="Nombre"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+              <small className="text-danger">{formErrors.name}</small>
+            </Form.Group>
 
-        {!isFormValid() && (
-          <Alert variant="danger" className="mt-3">
-            Por favor, complete todos los campos obligatorios.
-          </Alert>
-        )}
-      </Form>
-    </div>
+            <Form.Group>
+              <Form.Label>Email:</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+              <small className="text-danger">{formErrors.email}</small>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Check
+                type="checkbox"
+                name="isActive"
+                checked={formData.isActive}
+                onChange={handleChange}
+                label="Activo"
+                className="form-check"
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Contraseña:</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Contraseña"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+              <small className="text-danger">{formErrors.password}</small>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Check
+                type="checkbox"
+                name="isAdmin"
+                checked={formData.isAdmin}
+                onChange={handleChange}
+                label="Administrador"
+                className="form-check"
+              />
+            </Form.Group>
+
+            <Button
+              className='btn btn-success m-1'
+              onClick={handleFormSubmit}
+            >
+              {formData.editingUserId !== null ? 'Guardar' : 'Enviar'}
+            </Button>
+            <Button className='btn btn-secondary m-1' onClick={handleCancel}>
+              Cancelar
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
